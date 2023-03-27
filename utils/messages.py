68b82ba6434a8edc82e89ffa09fbcd05ddb98bc8
PrOn1x_ <101285149+Pr0n1xGH@users.noreply.github.com>
2023-03-27 23:09:16 +0500

    Update messages.py

--- utils/messages.py
+++ utils/messages.py
@@ -16,7 +16,7 @@ logo_message = f"{Fore.BLUE}{Style.BRIGHT} ___ ___ ___ ___ ___ ___ \n" \
                f"/ __| _ \ _ \_ _| _ ) __|  _ __ _  _  \n" \
                f"\__ \  _/   /| || _ \ _| _| '_ \ || | \n" \
                f"|___/_| |_|_\___|___/___(_) .__/\_, | \n" \
-               f"                   {Fore.RED}{Style.BRIGHT}v.1.4.0{Fore.BLUE}{Style.BRIGHT}|_|   |__/ \n" \
+               f"                   {Fore.RED}{Style.BRIGHT}v.1.0.0{Fore.BLUE}{Style.BRIGHT}|_|   |__/ \n" \
                f"{Fore.GREEN}{Style.BRIGHT}>>> Информация:\n" \
                f"{Fore.YELLOW}{Style.BRIGHT}Support: @devspribe\nTelegram: @tgscriptss\nTikTok: @tgscript\nВ других соц.сетях нас нет!\n\n" \
                f"{Fore.GREEN}{Style.BRIGHT}$ Скрипт запущен! Напишите {Fore.BLUE}{Style.BRIGHT}.help{Fore.GREEN}{Style.BRIGHT}(в чат телеграма) что-бы посмотреть доступные модули"
