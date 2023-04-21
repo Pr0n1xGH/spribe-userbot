b7565a73beaa61195a888cd3e1a2a31e7f82fa0f
PrOn1x_ <101285149+Pr0n1xGH@users.noreply.github.com>
2023-04-22 01:31:33 +0500

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -15,8 +15,10 @@ date_string = now.strftime(f"%Y-%m-%d_%H-%M")
 
 log_path = f"userbot/utils/misc/logs/logs-{date_string}.txt"
 
-with open(log_path, 'w+') as f:
-    logging.basicConfig(filename=log_path, level=logging.INFO)
+with open(log_path, 'w') as f:
+    f.write("-- logging --")
+
+logging.basicConfig(filename=log_path, level=logging.INFO)
 
 # launch point
 if sys.version_info < (3, 9, 0):
