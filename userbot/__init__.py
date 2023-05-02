882f49e2d6e064b97f35d1dc12f8ccf9b53499f9
PrOn1x_ <101285149+Pr0n1xGH@users.noreply.github.com>
2023-05-02 23:07:54 +0500

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -3,3 +3,7 @@
 # >> https://github.com/Pr0n1xGH/spribe-userbot
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # >> https://www.gnu.org/licenses/agpl-3.0.html
+
+import time
+
+start_time = time.time()
