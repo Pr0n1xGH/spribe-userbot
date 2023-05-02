de52d53863ea414a0b7602371fe2738de627a553
PrOn1x_ <101285149+Pr0n1xGH@users.noreply.github.com>
2023-05-02 17:55:01 +0500

    Add files via upload

---
+++ userbot/__init__.py
@@ -0,0 +1,5 @@
+# ©️ Spribe Userbot, 2023
+# This file is a part of Spribe Userbot
+# >> https://github.com/Pr0n1xGH/spribe-userbot
+# You can redistribute it and/or modify it under the terms of the GNU AGPLv3
+# >> https://www.gnu.org/licenses/agpl-3.0.html
