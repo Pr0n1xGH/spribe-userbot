d86d4182ee7ff8d42af3b8bce9a0c7c8601d42ce
PrOn1x_ <101285149+Pr0n1xGH@users.noreply.github.com>
2023-05-02 17:53:10 +0500

    Updated commands

--- userbot/plugins/default.py
+++ userbot/plugins/default.py
@@ -8,23 +8,25 @@ import asyncio
 import os
 import zipfile
 import glob
+import time
 from pathlib import Path
+from platform import python_version
 
 from pyrogram import Client, filters
+from pyrogram import __version__ as verpyro
 
 from .help import add_command_help
 from ..__main__ import logger
+from ..base import database
 
 
-@Client.on_message(
-    filters.command('loadmod', prefixes='.') & filters.me
-)
+@Client.on_message(filters.command('loadmod', prefixes='.') & filters.me)
 async def loadmod(client, message):
     reply_message = message.reply_to_message
 
     if not reply_message:
         await message.edit("<emoji id=5210952531676504517>🔴</emoji> ▸ В этом сообщении не обнаружено модуля.")
-        
+
     else:
         if reply_message.document:
             file_name = reply_message.document.file_name
@@ -45,20 +47,19 @@ async def loadmod(client, message):
             else:
                 await client.download_media(file_id, file_name=f'plugins/{file_name}')
 
-            await message.edit('<emoji id=5438274168422409988>⚙</emoji> ▸ Модуль успешно добавлен!\n\n<emoji id=5341350410252723241>🛠️</emoji> Перезагрузите скрипт командой -reload что-бы модули заработали.')
+            await message.edit(
+                '<emoji id=5438274168422409988>⚙</emoji> ▸ Модуль успешно добавлен!\n\n<emoji id=5341350410252723241>🛠️</emoji> Перезагрузите скрипт командой -reload что-бы модули заработали.')
         else:
             await message.edit('<emoji id=5210952531676504517>🔴</emoji> ▸ В этом сообщении не обнаружено модуля.')
 
     await asyncio.sleep(10)
     await message.delete()
-    
-    
-@Client.on_message(
-    filters.command('unloadmod', prefixes='.') & filters.me
-)
+
+
+@Client.on_message(filters.command('unloadmod', prefixes='.') & filters.me)
 async def unloadmod(client, message):
     name_module = ' '.join(message.command[1:])
-    
+
     try:
         if name_module.endswith(".py"):
             pass
@@ -74,27 +75,25 @@ async def unloadmod(client, message):
             else:
                 os.remove("modules/" + name_module)
                 await message.edit("<emoji id=5206607081334906820>🟢</emoji> ▸ Модуль был удалён!")
-                
+
         else:
             await message.edit("<emoji id=5210952531676504517>🔴</emoji> ▸ Такого модуля не существует.")
-            
+
     except Exception as e:
         await message.edit("<emoji id=5386757912607599167>🛠️</emoji> ▸ Произошла ошибка:\n\n" + e)
 
     await asyncio.sleep(10)
     await message.delete()
-    
-    
-@Client.on_message(
-    filters.command('backup', prefixes='.') & filters.me
-)
+
+
+@Client.on_message(filters.command('backup', prefixes='.') & filters.me)
 async def backup(client, message):
     await message.edit('<emoji id=5438274168422409988>⚙</emoji> ▸ Бэкап модулей. Подождите...')
 
-    zip_file = zipfile.ZipFile('modules.zip', 'w', zipfile.ZIP_DEFLATED)
+    zip_file = zipfile.ZipFile('plugins.zip', 'w', zipfile.ZIP_DEFLATED)
 
     modules = 0
-    for root, dirs, files in os.walk("modules/"):
+    for root, dirs, files in os.walk("userbot/plugins/"):
         for file in files:
             if file.endswith('.py'):
                 if file != '_example.py' and file != 'default.py' and file != 'help.py':
@@ -104,52 +103,51 @@ async def backup(client, message):
     zip_file.close()
     await message.delete()
 
-    with open("modules.zip", "rb") as file:
-        await client.send_document(chat_id=message.chat.id, document=file, caption=f"<emoji id=5449555539975481957>📦</emoji> ▸ Бэкап модулей ({modules})")
+    with open("plugins.zip", "rb") as file:
+        await client.send_document(chat_id=message.chat.id,
+                                   document=file,
+                                   caption=f"<emoji id=5449555539975481957>📦</emoji> ▸ Бэкап модулей ({modules})")
 
-    if os.path.isfile("modules.zip"):
-        os.remove("modules.zip")
-        
-        
-@Client.on_message(
-    filters.command('reload', prefixes='.') & filters.me
-)
+    if os.path.isfile("plugins.zip"):
+        os.remove("plugins.zip")
+
+
+@Client.on_message(filters.command('reload', prefixes='.') & filters.me)
 async def reload(client, message):
     block = ' '.join(message.command[1:])
-    
+
     try:
         await message.edit('<emoji id=5438274168422409988>🔄️</emoji> ▸ Скрипт перезагружается...')
-        await client.restart(block = block == "True")
+        await client.restart(block=block == "True")
         await message.edit('<emoji id=5438274168422409988>⚙</emoji> ▸ Скрипт перезагрузился.')
-        
+
     except Exception as e:
-        await message.edit(f'<emoji id=5438274168422409988>🔄️</emoji> ▸ Ошибка...\n\n<emoji id=5386757912607599167>🛠️</emoji> {e}')
-    
+        await message.edit(f'<emoji id=5438274168422409988>🔄️</emoji> ▸ Ошибка...\n\n<emoji '
+                           f'id=5386757912607599167>🛠️</emoji> {e}')
+
     await asyncio.sleep(10)
     await message.delete()
 
 
-@Client.on_message(
-    filters.command("logs", ".") & filters.me
-)
-async def logs(client, message):
+@Client.on_message(filters.command("logs", ".") & filters.me)
+async def _logs(client, message):
     cmds = ' '.join(message.command[1:])
-    
+
     await message.edit(f"<emoji id=5438274168422409988>🔄️</emoji> Обработка...")
     if cmds == "all":
         zip_name = 'logs.zip'
-        
+
         with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
             for root, dirs, files in os.walk('logs'):
                 for file in files:
                     zipf.write(os.path.join(root, file))
-        
+
         with open(zip_name, "rb") as file:
             await message.delete()
             await client.send_document(
-                chat_id = message.chat.id, 
-                document = file, 
-                caption = f"`logs from Spribe-Userbot`"
+                chat_id=message.chat.id,
+                document=file,
+                caption=f"`logs from Spribe-Userbot`"
             )
 
         if os.path.isfile(zip_name):
@@ -157,8 +155,8 @@ async def logs(client, message):
 
     else:
         logs_dir = glob.glob("logs/*")
-        latest_file = max(logs_dir, key = os.path.getctime)
-        
+        latest_file = max(logs_dir, key=os.path.getctime)
+
         try:
             file_text = Path(latest_file).read_text(encoding='utf-8')
         except UnicodeDecodeError:
@@ -167,9 +165,9 @@ async def logs(client, message):
         if len(file_text) > 3000:
             await message.delete()
             await client.send_document(
-                chat_id = message.chat.id,
-                document = latest_file,
-                caption = "`logs from Spribe-Userbot`"
+                chat_id=message.chat.id,
+                document=latest_file,
+                caption="`logs from Spribe-Userbot`"
             )
 
         else:
@@ -177,6 +175,37 @@ async def logs(client, message):
 
     logger.info("Были взяты логи командой .logs")
     
+    
+@Client.on_message(filters.command("info", ".") & filters.me)
+async def inf(client, message):
+    from ..__main__ import start_time
+    from ..utils import messages
+    
+    uptime = time.time() - start_time
+    modules = 0
+    for root, dirs, files in os.walk("userbot/plugins/"):
+        for file in files:
+            if file.endswith('.py'):
+                if (file != '_example.py' 
+                    and file != 'default.py' 
+                    and file != 'help.py'
+                    ):
+                    modules += 1
+    
+    await message.edit(
+        f"🍃 **`Spribe-Userbot`**\n\n"
+        f"**🛠️ Пользователь**: `{client.me.mention}`\n"
+        f"**├ Кол-во модулей**: `{modules}` \n"
+        f"**├ Версия юзербота**: `{messages.Version}` \n"
+        f"**├ Версия Python**: `{python_version()}` \n"
+        f"**├ Версия Pyrogram**: `{verpyro}` \n"
+        f"**└ Время работы бота**: `{database.display_time(uptime)}` \n\n"
+        f"**🕛 Первый запуск юзербота**: `{database.get_fdate()[0]}`\n"
+        f"**└ Прошло времени**: `{database.get_ftime()}` \n"
+    )
+    await asyncio.sleep(20)
+    await message.delete()
+
 add_command_help(
     "default",
     [
@@ -184,6 +213,7 @@ add_command_help(
         [".unloadmod [Название модуля]", "Удаляет модуль"],
         [".backup", "Делает бэкап всех модулей"],
         [".logs [all]", "Даёт логи последнего запуска скрипта(без all) / Даёт zip файл со всеми логами"],
+        [".info", "Предостовляет информацию о юзерботе"]
         [".reload", "Перезагружает скрипт"],
     ],
 )
