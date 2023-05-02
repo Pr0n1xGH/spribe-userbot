192575f3431320fa2eb675a7f6dcf981d2fbe736
PrOn1x_ <101285149+Pr0n1xGH@users.noreply.github.com>
2023-05-02 23:17:53 +0500

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -20,24 +20,19 @@ async def help_command_handler(client: Client, message: Message):
     if len(cmd) > 1:
         module_name = " ".join(cmd[1:])
         await send_help_message(message, module_name)
-        
     elif message.reply_to_message:
         try:
             help_arg = message.reply_to_message.text
-            
             if help_arg in CMD_HELP:
                 module_name = (help_arg.split("\n")[0].strip().replace("Информация о ", "").replace(":", ""))
                 await send_help_message(message, module_name) 
             else:
                 await send_help_message(message)
-                
         except Exception as e:
             print(f"<emoji id=5210952531676504517>🔴</emoji> Ошибка: {e}")
             logger.error(f"help_command_handler: {e}")
-            
     else:
         await send_help_message(message)
-
     await asyncio.sleep(20)
     await message.delete()
 
@@ -57,25 +52,17 @@ def split_list(input_list: list, n: int) -> list[list]:
 
 async def send_help_message(message: Message, module_name: str = None) -> None:
     if module_name:
-        
         if module_name in CMD_HELP:
-            
             commands = CMD_HELP[module_name]
             module_help = f"<emoji id=5443132326189996902>🍃</emoji> __Информация о__ **{module_name}**:<a href=\"https://i.ibb.co/Gv14bhw/photo-2023-04-10-15-16-04.jpg\">&#8203;</a>\n\n"
-            
             for command, description in commands.items():
                 module_help += f"**`{command}`** \n└ __{description}__\n"
-                
             await message.edit_text(module_help)
-            
         else:
-            await message.edit_text("`<emoji id=5210952531676504517>🔴</emoji> Пожалуйста, укажите нормальное имя модуля.`")
-            
+            await message.edit_text("`<emoji id=5210952531676504517>🔴</emoji> Пожалуйста, укажите нормальное имя модуля.`")  
     else:
         all_commands = "**<emoji id=5447410659077661506>🛠️</emoji> __Список модулей__:**\n\n"
-        
         for module_group in split_list(sorted(CMD_HELP.keys()), 2):
             all_commands += ("• " + '\n• '.join(map(str, ["`" + cmd + "`" for cmd in module_group])) + "\n")
-            
         all_commands += "\n<emoji id=5397782960512444700>⚙</emoji> __Чтобы получить информацию по определенному модулю, используйте:__ `.help [Название модуля]`<a href=\"https://i.ibb.co/YW6RmJL/photo-2023-04-10-15-15-57.jpg\">&#8203;</a>"
         await message.edit_text(all_commands)
