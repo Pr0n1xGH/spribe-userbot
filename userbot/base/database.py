3140c55388be1248ef1a80b688bb4aeabc4430a5
PrOn1x_ <101285149+Pr0n1xGH@users.noreply.github.com>
2023-05-05 21:57:31 +0500

    Update database.py

--- userbot/base/database.py
+++ userbot/base/database.py
@@ -9,53 +9,65 @@ import sqlite3
 import time
 import datetime
 
-conn = sqlite3.connect(r'userbot/base/base.db')
-cur = conn.cursor()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS user(
-   first_start TEXT,
-   first_time REAL);
-""")
-conn.commit()
+class basetime():
+    def __init__(self) -> None:
+        self.conn = sqlite3.connect(r'userbot/base/databases/datatime.db')
+        self.cur = self.conn.cursor()
+        
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS user(
+            first_start TEXT,
+            first_time REAL);
+        """)
+        self.conn.commit()
+        
+    def get_fdate(self) -> any:
+        self.fdate = self.conn.execute(f'SELECT first_start FROM user').fetchone()
+        
+        return self.fdate
+    
+    def get_ftime(self) -> any:
+        ftime = self.conn.execute(f'SELECT first_time FROM user').fetchone()
+        secs = time.time() - float(ftime[0])
+        
+        return self.display_time(secs)
+    
+    def display_time(self, seconds, granularity=3) -> any:
+        """Turns seconds into a more pleasant view
 
-check = cur.execute("SELECT first_start FROM user")
+        Args:
+            - seconds (int): seconds
+            - granularity (int, optional): maximum output of internals. Defaults to 3.
+
+        Returns:
+            Turns it into finished text
+        """
+        intervals = (
+            ('нед', 604800),
+            ('дн', 86400),
+            ('ч', 3600),
+            ('м', 60),
+            ('с', 1),
+        )
+        
+        result = []
+        for name, count in intervals:
+            value = seconds // count
+            if value:
+                seconds -= value * count
+                if value == 1:
+                    name = name.rstrip('s')
+                result.append("{} {}".format(value, name))
+        return ', '.join(result[:granularity])
+
+check = basetime().cur.execute("SELECT first_start FROM user")
 
 if check.fetchone() is None: 
     first_time = time.time()
     
     now = datetime.datetime.now()
     first_date = now.strftime(f"%d.%m.%Y %H:%M")
+    dt = basetime()
     
-    cur.execute("INSERT INTO user VALUES(?, ?);", (first_date, first_time))
-    conn.commit()
-    
-    
-def get_fdate() -> any:
-    fdate = conn.execute(f'SELECT first_start FROM user').fetchone()
-    
-    return fdate
-
-def get_ftime() -> any:
-    ftime = conn.execute(f'SELECT first_time FROM user').fetchone()
-    secs = time.time() - float(ftime[0])
-    
-    return display_time(secs)
-
-def display_time(seconds, granularity=2):
-    intervals = (
-        ('нед', 604800),
-        ('дн', 86400),
-        ('ч', 3600),
-        ('м', 60),
-        ('с', 1),
-    )
-    
-    result = []
-    for name, count in intervals:
-        value = seconds // count
-        if value:
-            seconds -= value * count
-            if value == 1:
-                name = name.rstrip('s')
-            result.append("{} {}".format(value, name))
-    return ', '.join(result[:granularity])
+    dt.cur.execute("INSERT INTO user VALUES(?, ?);", (first_date, first_time))
+    dt.conn.commit()
